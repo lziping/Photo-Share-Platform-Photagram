cdf70a6c6aa6909756ee50f55c65be3a7d3f9bd9
lziping <77389522+lziping@users.noreply.github.com>
2022-08-24 01:55:13 -0400

    update home

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,4 +1,36 @@
 $(document).ready(function () {
+    $('#slides').superslides({
+        animation: "fade",
+        play: "5000" //图片替换时间 ms 1s=ms*1000
+    })
+    var t = new Typed(".slides-sub", {
+        strings: ["Welcome to Photagram! " +
+        "Feel free to share some of your own and comment on others!"],
+        typeSpeed: 70,
+        loop: true,
+        startDelay: 1000,
+        showCursor: false,
+    });
+    $('.owl-carousel').owlCarousel({
+        // loop: true,
+        // autoplay: true,
+        // autoplayTimeout: 2500,
+        // autoplayHoverPause: true,
+        items: 4,
+        animateOut: 'fadeOut',
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
+            }
+        }
+    })
+
 
     // $('#navigation li a').click(function (e) {
     //     e.preventDefault()
